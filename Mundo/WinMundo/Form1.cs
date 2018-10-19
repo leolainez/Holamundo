99e9c5c4965de4f4c1228bf50f20de1cbcdd0d81
Leolainez <meraelizabeth_93@hotmail.com>
2018-10-18 22:35:21 -0600

    Agregando boton para saludar

--- Mundo/WinMundo/Form1.cs
+++ Mundo/WinMundo/Form1.cs
@@ -16,5 +16,10 @@ namespace WinMundo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Mundo");
+        }
     }
 }
